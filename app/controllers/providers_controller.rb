988c9184c00bd66a65a860853225923fe41841d8
Matt Blake <myblake@gmail.com>
2023-05-31 09:34:29 -0400

    minor refactor for controller and views

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,53 +2,33 @@ require 'finch_client'
 
 class ProvidersController < ApplicationController
     before_action :providers_list
+    before_action :set_provider_and_access_token, except: :index
 
     def index
     end
 
-    # need to handle the 501 method not implemented cases properly
-
     def show
-        @provider = params[:provider_id]
-        client = FinchClient.new
-
-        # todo gross
-        @resp = nil
-        access_token = Rails.cache.fetch("access_token/#{@provider}") do    
-            @resp = client.create_sandbox(@provider)
-            @resp["access_token"]
-        end
-        client.set_auth_token(access_token)
-
-        @information = client.get_information
-        @directory = client.get_directory
+        @information = @client.get_information
+        @directory = @client.get_directory
         
-        # TODO handle error better
-        @employee_names = {}
-        @directory["individuals"].each do |individual| 
-            ee_name = "#{individual['first_name']} #{individual['last_name']}"
-            @employee_names[individual["id"]] = ee_name
-        end
-        
+        # if directory exists build name dictionary for manager name
+        build_employee_map
     end
 
     def individual
-        @provider = params[:provider_id]
         @individual_id = params[:individual_id]
 
-        client = FinchClient.new
-        
-        # todo move this code to client
-        @resp = nil
-        access_token = Rails.cache.fetch("access_token/#{@provider}") do    
-            @resp = client.create_sandbox(@provider)
-            @resp["access_token"]
-        end
-        client.set_auth_token(access_token)
-
         # TODO handle this better
-        @individual = client.get_individual(@individual_id)['responses'].first['body']
-        @employment = client.get_employment(@individual_id)['responses'].first['body']
+
+        # when restarting server and IDS invalid this object is a nicely formatted error
+        # e.g. {"error_name"=>"invalid_request_error", "error_message"=>"No individual with id bc7ccc0b-f269-4baa-9627-ab80cc3249f0"}
+
+        # can assume likely the same issue on not implemented
+        # @individual['code'] == 501 and @individual['name'] == 'not_implemented_error'
+
+        @individual = @client.get_individual(@individual_id)['responses'].first['body']
+        @employment = @client.get_employment(@individual_id)['responses'].first['body']
+        # debugger
     end
 
     private
@@ -61,5 +41,27 @@ class ProvidersController < ApplicationController
             paychex_flex: "Paychex Flex",
             workday: "Workday"
         }
+    end
+
+    def set_provider_and_access_token
+        @provider = params[:provider_id]
+        @client = FinchClient.new
+
+        @sandbox_create_response = nil
+        access_token = Rails.cache.fetch("access_token/#{@provider}") do
+            @sandbox_create_response = @client.create_sandbox(@provider)
+
+            # handle errors here
+            @sandbox_create_response["access_token"]
+        end
+        @client.set_auth_token(access_token)
+    end
+
+    def build_employee_map
+        @employee_names = {}
+        @directory["individuals"].try(:each) do |individual|
+            full_name = [individual['first_name'],individual['middle_name'], individual['last_name']].join(' ')
+            @employee_names[individual["id"]] = full_name
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/providers/', to: 'providers#index'
-  get '/providers/:provider_id', to: 'providers#show'
-  get '/providers/:provider_id/individuals/:individual_id', to: 'providers#individual'
+  get '/providers/:provider_id', to: 'providers#show', as: :provider_show
+  get '/providers/:provider_id/individuals/:individual_id', to: 'providers#individual', as: :individual_show
 end
