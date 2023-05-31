a73313aa32317c18d52020dec96d43c08b1e58a4
Matt Blake <myblake@gmail.com>
2023-05-30 20:32:24 -0400

    empty rails project

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/app/layout.tsx": [
+      "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/app/providers/page": [
+      "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/app/globals.css",
+      "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/app/providers/[provider_id]/page": [
+      "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/app/globals.css",
+      "/Users/mattblake/Workspace/finch-se-interview/mb-finch-api/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
